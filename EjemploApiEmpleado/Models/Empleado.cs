8fe354afc37b6a7ac0fbc1868163532cdf938af3
toni121212 <rivasgonzalezluisantonio@gmail.com>
2026-02-06 19:20:42 -0600

    Add project files.

---
+++ EjemploApiEmpleado/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿namespace EjemploApiEmpleado.Models
+{
+    public class Empleado
+    {
+
+        public int id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public int NEmpleado { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+        
+        public bool Honorarios { get; set; }
+
+    }
+}
---
+++ EjemploApiEmpleado/Models/EmpleadoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EjemploApiEmpleado.Models
+{
+    public class EmpleadoContext: DbContext
+    {
+        public EmpleadoContext (DbContextOptions<EmpleadoContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; } = null!;
+    }
+}
